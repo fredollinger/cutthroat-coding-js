aa0c7a56670cb0cff6e608fee94b75fa1254dacf
Frederick.Ollinger <follinge@gmail.com>
2023-09-15 18:05:20 -0700

    CamelCase() Complete

--- strings.js
+++ strings.js
@@ -1,6 +1,17 @@
 // Tell if two arrays are equal
 function CamelCase(str) {
-    var res = "result";
+    var res = str.toLowerCase().charAt(0);
+    for (let i = 1; i < str.length; i++) {
+        if ("-" == str.charAt(i)) {
+            i = i + 1;
+            res = res + str.toUpperCase().charAt(i);
+        }
+        else {
+            res = res + str.charAt(i);
+        }
+        console.log(str.charAt(i));
+    };
+
     return res;
 }
 
