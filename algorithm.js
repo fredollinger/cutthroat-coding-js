9442de8b99bb3b8e50746e3a7b3eb817eb650268
Frederick.Ollinger <follinge@gmail.com>
2024-12-10 10:02:55 -0800

    Add BinarySearch Exception STUB

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,30 @@
 function binarySearch(arr, item){
-    for(let i = 0; i < arr.length; i++) {
-     
+    var low = 0;
+    var high = arr.length;
+    var mid = (high - low) / 2;
+    while (low < high) {
+	console.log("low: " + low + " mid: " + mid + " high: " + high);
+        if (arr[mid] == item )
+        {
+            break;
+        }
+        else if (item > arr[mid])
+        {
+            low = mid;
+            mid = low + (high - low) / 2;
+        }
+        else if (item < arr[mid])
+        {
+            high = mid;
+            mid = (high - low) / 2;
+        }
+	else {
+	    console.log("arr[mid]: " + arr[mid]);
+	    console.log("low: " + low + " mid: " + mid + " high: " + high);
+	    throw new Error("Should never get here");
+	}
     }
+    return mid;
 }
 
 function bruteForceSearch(arr, item){
