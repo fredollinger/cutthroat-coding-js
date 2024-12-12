a133d3c74c908ca981b89207c4c7d5f179b5fc72
Frederick.Ollinger <follinge@gmail.com>
2024-12-11 17:03:56 -0800

    Solve Binary Search 3rd Time

--- algorithm.js
+++ algorithm.js
@@ -7,22 +7,18 @@ function binarySearch(arr, item){
 	mid = Math.floor(mid)
         if (arr[mid] == item )
         {
-	    res = arr[mid];
+	        res = arr[mid];
             break;
         }
         else if (item > arr[mid])
         {
-	    console.log("item: " + item + " low: " + low + " mid: " + mid + " high: " + high);
             low = mid + 1;
             mid = (high + low) / 2;
-	    console.log("mid: " + mid);
         }
         else if (item < arr[mid])
         {
-	    console.log("item: " + item + " low: " + low + " mid: " + mid + " high: " + high);
             high = mid - 1;
             mid = (high + low) / 2;
-	    console.log("mid: " + mid);
         }
 	else {
 	    console.log("arr[mid]: " + arr[mid]);
@@ -35,7 +31,60 @@ function binarySearch(arr, item){
 
 function binarySearch1(arr, item){
     var res = -1;
+    var low = 0;
+    var high = arr.length;
+    var mid = high / 2;
+
+    while (high > low) {
+        mid = Math.floor(mid);
+        if (item == arr[mid]) {
+            res = arr[mid];
+            break; 
+        }
+        else if (item > arr[mid]) {
+	    console.log("item: " + item + " low: " + low + " mid: " + mid + " high: " + high);
+            low = mid + 1;
+            mid = (high + low) / 2;
+	    console.log("mid: " + mid);
+        }
+        else if (item < arr[mid]) {
+	    console.log("item: " + item + " low: " + low + " mid: " + mid + " high: " + high);
+            high = mid - 1;
+            mid = (high + low) / 2;
+	    console.log("mid: " + mid);
+        }
+        else { 
+            throw new Error("we should not get here");
+        }
+    }
+
     return res;
+}
+
+function binarySearch2(arr, item){
+    var low = 0;
+    var high = arr.length;
+    var mid = (high - low) / 2;
+    var res = -1;
+    while (low < high) {
+	// ensure int for arr index
+	mid = Math.floor(mid);
+        if (item == arr[mid]) {
+	    return mid;
+	}
+        else if (item > arr[mid]) {
+	    low = mid + 1;
+	    mid = (high + low) / 2; 
+	}
+        else if (item < arr[mid]) {
+	    high = mid - 1;
+	    mid = (high + low) / 2; 
+	}
+	else {
+	    throw new Error("thould never get here");
+	}
+    }
+    return -1;
 }
 
 function bruteForceSearch(arr, item){
@@ -51,5 +100,6 @@ function bruteForceSearch(arr, item){
 module.exports = {
     binarySearch,
     binarySearch1,
+    binarySearch2,
     bruteForceSearch
 }
